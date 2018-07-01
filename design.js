bbf75375667765a11a895dadf86e59cef7a50d5e
irerilewis <40041973+irerilewis@users.noreply.github.com>
2018-07-01 23:48:24 +0300

    Add files via upload

---
+++ design.js
@@ -0,0 +1,38 @@
+$(function(){
+	var height, width, color;
+
+// When size is submitted by the user, call makeGrid()
+
+	$('#sizePicker').submit(function (event) {
+    event.preventDefault();
+    height = $('#inputHeight').val();
+    width = $('#inputWeight').val();
+    makeGrid(height, width);
+})
+
+function makeGrid(x, y) {
+
+  // Your code goes here!
+  $('tr').remove();
+  for (var i = 1; i <= x; i++) {
+      $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+      for (var j = 1; j <=y; j++) {
+          $('#table' + i).append('<td></td>');
+        }
+    }
+
+    // add colors to the cells
+  $('td').click(function addColor(){
+      color = $('#colorPicker').val();
+
+      if ($(this).attr('style')) {
+          $(this).removeAttr('style')
+      } else {
+          $(this).attr('style', 'background-color:' + color);
+      }
+  });
+}
+$('input').on('click', function(){
+	$('body').css('background-color', '#FFD2F2')
+});
+});
